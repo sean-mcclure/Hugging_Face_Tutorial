bf81938d3d7a4ea0f031a67f6e1db22d69aab9dc
Sean McClure <seanaaron100@gmail.com>
2021-10-06 18:58:48 +0000

    working on sequences

--- app/hugging_face_app/src/App.js
+++ app/hugging_face_app/src/App.js
@@ -2,11 +2,10 @@ import "./App.css";
 
 import {config} from "./config.js";
 
-
 function call_api() {
-    const tweets = ["walking to the park", "finding the store", "and this and that", "what about this", "finally we will see"]
-    console.log("https://" + config.api_port + "-" + config.api_url + "/" + config.api_endpoint + "?Sequences=" + encodeURI(tweets))
-    fetch("https://" + config.api_port + "-" + config.api_url + "/" + config.api_endpoint + "?Sequences=" + encodeURI(tweets))//.join(","))
+    const tweets = ["walking to the park", "finding the store", "and this and that", "what about this", "finally we will see"];
+    console.log("https://" + config.api_port + "-" + config.api_url + "/" + config.api_endpoint + "?Sequences=" + tweets.join("&Sequences="));
+    fetch("https://" + config.api_port + "-" + config.api_url + "/" + config.api_endpoint + "?Sequences=" + tweets.join("&Sequences=")) //.join(","))
     .then(response => response.json())
     .then(data => console.log(data))
     .catch((error) => {
