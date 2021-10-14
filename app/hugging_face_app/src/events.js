28c92543992027728b267cf9023ef775cf97adec
Sean McClure <seanaaron100@gmail.com>
2021-10-14 15:22:09 +0000

    adding clean_tweets to events.js2

--- app/hugging_face_app/src/events.js
+++ app/hugging_face_app/src/events.js
@@ -6,9 +6,6 @@ import {
 export const events = {
   call_api: function() {
       const tweets = events.read_tweets();
-      console.log(tweets)
-      console.log("https://" + config.api_port + "-" + config.api_url + "/" + config.api_endpoint + "?Sequences=" + encodeURI(tweets.join("&Sequences=")))
-     // const tweets = ["I am angry about the pizza", "I am happy about the pizza", "let's have some dinner together"];
       fetch("https://" + config.api_port + "-" + config.api_url + "/" + config.api_endpoint + "?Sequences=" + encodeURI(tweets.join("&Sequences=")))
       .then(response => response.json())
       .then(data => events.array_occurences(data.Predictions))
@@ -34,13 +31,13 @@ export const events = {
       for(const num of arr) {
           counts[num] = counts[num] ? counts[num] + 1 : 1;
       }
-      console.log(counts)
       events.draw_plot(counts);
   },
   draw_plot : function(counts) {
-      document.getElementById("bar_1").style.height = counts["anger"] + "%";
-      document.getElementById("bar_2").style.height = counts["joy"] + "%";
-      document.getElementById("bar_3").style.height = counts["optimism"] + "%";
-      document.getElementById("bar_4").style.height = counts["sadness"] + "%";
+      const total = Object.values(counts).reduce((a, b) => a + b);
+      document.getElementById("bar_1").style.height = (counts["anger"]/total)*100 + "%";
+      document.getElementById("bar_2").style.height = (counts["joy"]/total)*100 + "%";
+      document.getElementById("bar_3").style.height = (counts["optimism"]/total)*100 + "%";
+      document.getElementById("bar_4").style.height = (counts["sadness"]/total)*100 + "%";
   }
 }
